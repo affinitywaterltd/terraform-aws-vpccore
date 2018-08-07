3c53687d2a2d5324a78e9e6abe69aa696a540745
Ellis <tim.ellis@affinitywater.co.uk>
2018-08-07 13:44:09 +0100

    .

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -1,14 +1,13 @@
 resource "aws_db_subnet_group" "group" {
-  name       = "database_subnet"
-  subnet_ids = ["${aws_subnet.private.*.id}"]
+  name        = "database_subnet"
+  subnet_ids  = ["${aws_subnet.private.*.id}"]
   description = "All private instances for RDS placement"
 
   tags {
-    Name        = "DB Subnet Group"
-
+    Name = "DB Subnet Group"
   }
 }
 
-output "db_subnet_group" {
+output "db_subnet_name" {
   value = "${aws_db_subnet_group.group.*.name}"
 }
