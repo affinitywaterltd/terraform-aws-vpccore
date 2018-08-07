b28fc18eac1bce4d5062c90cd5a9161fbadf4428
Ellis <tim.ellis@affinitywater.co.uk>
2018-08-07 11:55:11 +0100

    Put description in the right place

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -1,10 +1,11 @@
 resource "aws_db_subnet_group" "group" {
   name       = "database_subnet"
   subnet_ids = ["${aws_subnet.private.*.id}"]
+  description = "All private instances for RDS placement"
 
   tags {
     Name        = "DB Subnet Group"
-    Description = "All private instances for RDS placement"
+
   }
 }
 
