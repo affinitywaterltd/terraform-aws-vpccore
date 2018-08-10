978c17301eb845470778f8a180697d488d8bd41e
Ellis <tim.ellis@affinitywater.co.uk>
2018-08-10 11:58:21 +0100

    Added variable into DB Subnet name since it needs to be unique

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "group" {
-  name        = "database_subnet"
+  name        = "database_subnet_${var.env}"
   subnet_ids  = ["${aws_subnet.private.*.id}"]
   description = "All private instances for RDS placement"
 
