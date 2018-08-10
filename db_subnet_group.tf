c0affbd53ab97017bb57f54c54f18e0e0621e588
Ellis <tim.ellis@affinitywater.co.uk>
2018-08-10 12:12:20 +0100

    .

--- db_subnet_group.tf
+++ db_subnet_group.tf
@@ -1,5 +1,5 @@
 resource "aws_db_subnet_group" "group" {
-  name        = "database_subnet_${var.vpc_environment_tag}"
+  name        = "database_subnet_${var.Environment}"
   subnet_ids  = ["${aws_subnet.private.*.id}"]
   description = "All private instances for RDS placement"
 
